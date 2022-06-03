1fb6f1a39ee541cd55f98dd76a67f6be3eee51ea
jack <1098894806@qq.com>
2022-06-03 23:01:26 +0800

    first commit

---
+++ src/main/java/com/gu/yanzhao/YanzhaoApplication.java
@@ -0,0 +1,13 @@
+package com.gu.yanzhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanzhaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanzhaoApplication.class, args);
+	}
+
+}
