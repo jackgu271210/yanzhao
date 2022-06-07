05b05c7e803ceb286fcc3bc6e3d14976107c134b
jackgu271210 <1098894806@.com>
2022-06-07 16:42:01 +0800

    updated

--- src/main/resources/static/assets/js/country.js
+++ src/main/resources/static/assets/js/country.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
     $('#editButton').on('click',function(event) {
-        console.log('sussess');
+        // console.log('sussess');
+        // $('#editModal').modal();
+        var href = $(this).attr('href');
+        $.get(href,function(country,status) {
+            $('#idEdit').val(country.id);
+            $('#descriptionEdit').val(country.description);
+            $('#capitalEdit').val(country.capital);
+            $('#codeEdit').val(country.code);
+            $('#continentEdit').val(country.continent);
+            $('#nationalityEdit').val(country.nationality);
+        });
         $('#editModal').modal();
     })
 })
